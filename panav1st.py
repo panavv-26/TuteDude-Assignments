53d4f5c3899b985dddb4d6594681afb3d886ee11
panavv-26 <panavwalia498@gmail.com>
2025-05-15 10:49:53 +0530

    Add files via upload

---
+++ panav1st.py
@@ -0,0 +1,15 @@
+#ASSIGNMENT 1
+#Task 1
+
+a=input('Enter the first number:')
+a=float(a)
+b=input('Enter the second number:')
+b=float(b)
+c =(a+b)
+print('c',c)
+
+#TASK 2
+a = input('Enter your first name: ')
+b = input('Enter your last name: ')
+c = a + " "+ b
+print("Hello,", c, "! \t Welcome to the Python Program")
